b7c0cdd06c459fe9421923c1d7b9f213562330d5
Vladyslav Spirin <vladspirin@hotmail.com>
2024-11-06 12:36:14 +0200

    Added TC1, looks ok

--- lesson_09/homeworks.py
+++ lesson_09/homeworks.py
@@ -12,43 +12,9 @@ Import and the tests themselves
 should be placed in a separate file - test_homeworks08.py.
 The evaluation will depend on both the quality of the tests and
 the amount of test coverage.
-A minimum of 10 points requires one properly designed positive test for a function.
+A minimum of 10 points requires one properly designed
+positive test for a function.
 """
-
-def arithmetic_mean(lst):
-    """
-    Calculate arithmetic mean of the numbers list.
-
-    Args:
-        lst (list): list of numbers.
-    Returns:
-        float: returns arithmetic mean.
-    """
-    return (sum(lst) / len(lst))
-
-
-def longest_word(lst):
-    """
-    Find and return longest word in the list.
-
-    Args:
-        lst (list): list with words.
-    Returns:
-        str: longest word in the list.
-    """
-    return max(lst, key=lambda word: len(word))
-
-
-def even_nums_sum(lst):
-    """
-    Filter even numbers in list and sum them.
-
-    Args:
-        lst (list): list of numbers.
-    Returns:
-        int: sum of even numbers
-    """
-    return sum([num for num in lst if num % 2 == 0])
 
 
 def perimetery(a, b, c, d):
@@ -64,3 +30,39 @@ def perimetery(a, b, c, d):
         int: perimetery of the figure.
     """
     return (a + b + c + d)
+
+
+def longest_word(lst):
+    """
+    Find and return longest word in the list.
+
+    Args:
+        lst (list): list with words.
+    Returns:
+        str: longest word in the list.
+    """
+    return max(lst, key=lambda word: len(word))
+
+
+def arithmetic_mean(lst):
+    """
+    Calculate arithmetic mean of the numbers list.
+
+    Args:
+        lst (list): list of numbers.
+    Returns:
+        float: returns arithmetic mean.
+    """
+    return (sum(lst) / len(lst))
+
+
+def even_nums_sum(lst):
+    """
+    Filter even numbers in list and sum them.
+
+    Args:
+        lst (list): list of numbers.
+    Returns:
+        int: sum of even numbers
+    """
+    return sum([num for num in lst if num % 2 == 0])
